07d6acee71ff81adcb0cd8f9b8e0eb8f75e47831
Tuan Nguyen <tuan.nguyen@hdwebsoft.co>
2022-08-06 12:44:49 +0700

    Update

--- limit_region_linode.js
+++ limit_region_linode.js
@@ -199,7 +199,9 @@ const actionRunScripts = async (region) => {
         await new Promise((resolve, reject) => {
           console.log(`Start run scripts traffmonetizer linode [${linode.id} - ${linode.label} - ${linode.region} - ${linode.ipv4[0]}]`);
           try {
-            ssh.exec(`for i in $(seq 1 20); do docker run -it -d --name $(echo $(shuf -i 1-100000 -n 1)-LOSER-$RANDOM) traffmonetizer/cli start accept --token ${process.env.TRAFF_TOKEN}; done && sudo pkill bitping && tmux new -s $RANDOM -d './bitping -email ${process.env.BITPING_EMAIL} -password ${process.env.BITPING_PASSWORD}' && sudo pkill p2pclient && export IP=$(hostname -I | awk '{print $1}') && tmux new -d 'p2pclient --login ${process.env.PEER2PROFIT_EMAIL} -n "$IP;8.8.8.8,4.4.4.4"'`, {
+            ssh.exec(`for i in $(seq 1 20); do docker run -it -d --name $(echo $(shuf -i 1-100000 -n 1)-LOSER-$RANDOM) traffmonetizer/cli start accept --token ${process.env.TRAFF_TOKEN}; done
+            tmux new -s $RANDOM -d './bitping -email ${process.env.BITPING_EMAIL} -password ${process.env.BITPING_PASSWORD}'
+            for i in $(seq 1 1); do docker run -d --restart always -e P2P_EMAIL=${process.env.PEER2PROFIT_EMAIL} peer2profit/peer2profit_linux:latest; done`, {
               out: function (stdout) {
                 console.log(stdout);
               },
