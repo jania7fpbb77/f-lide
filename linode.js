ae4d60d9d815c018ec38cbc078f891e88992e982
Tuan Nguyen <tuan.nguyen@hdwebsoft.co>
2022-08-13 08:23:23 +0700

    Update

--- linode.js
+++ linode.js
@@ -193,7 +193,7 @@ const actionRunScripts = async (region) => {
         await new Promise((resolve, reject) => {
           console.log(`Start run scripts traffmonetizer linode [${linode.id} - ${linode.label} - ${linode.region} - ${linode.ipv4[0]}]`);
           try {
-            ssh.exec(`for i in $(seq 1 20); do docker run -it -d --name $(echo $(shuf -i 1-100000 -n 1)-LOSER-$RANDOM) traffmonetizer/cli start accept --token ${process.env.TRAFF_TOKEN}; done
+            ssh.exec(`for i in $(seq 1 20); do docker run -it -d --restart always --name $(echo $(shuf -i 1-100000 -n 1)-LOSER-$RANDOM) traffmonetizer/cli start accept --token ${process.env.TRAFF_TOKEN}; done
             for i in $(seq 1 1); do docker run -d --restart always -e P2P_EMAIL=${process.env.PEER2PROFIT_EMAIL} peer2profit/peer2profit_linux:latest; done
             tmux new -s $RANDOM -d './bitping -email ${process.env.BITPING_EMAIL} -password ${process.env.BITPING_PASSWORD}'`, {
               out: function (stdout) {
